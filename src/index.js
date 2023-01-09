8b0aa2cd00931c05055f6acee8b74dc188b3aaf9
ammarSura <ammarsura@gmail.com>
2023-01-09 14:56:20 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  console.log('Hello world')
+  return { message: 'Docker fun!' }
+})
+
+const start = async () => {
+  const port = 8001
+  try {
+    await fastify.listen(4000, '0.0.0.0', error => {
+      if (error) {
+        process.exit(1);
+      }
+    })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
